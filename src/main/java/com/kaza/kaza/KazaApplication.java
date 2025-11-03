d9e1cf840bc185ce6ee4ce2fccb7bd88fa04445d
Henry Bentil <hcbentil@acsint.io>
2025-11-03 08:17:45 +0000

    first commit

---
+++ src/main/java/com/kaza/kaza/KazaApplication.java
@@ -0,0 +1,13 @@
+package com.kaza.kaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaza/kaza/KazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaza.kaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
